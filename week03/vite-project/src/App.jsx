67db3fdd2b2ac008a79a8431caea1e9b78f32920
KIM JEONGMIN <wjdals030328@catholic.ac.kr>
2025-04-26 16:39:05 +0900

    feat:버튼 추가

--- week03/vite-project/src/App.jsx
+++ week03/vite-project/src/App.jsx
@@ -1,28 +1,21 @@
 // App.jsx
 
-import { useState } from "react";
 import Card from "./components/Card";
 import Header from "./components/Header";
-import Search from "./components/Search";
-
 import { members } from "./mock/member";
+import Search from "./components/Search";
+import useSearch from "./hooks/useSearch";
 
 function App() {
-    const [search, setSearch] = useState("");
-
-    const handleSearch = (e) => {
-        setSearch(e.target.value);
-    };
-
-    const filteredMembers = members.filter((member) => member.name.includes(search));
+    const { search, filteredMembers, handleSearchChange, handleSearch } = useSearch(members);
 
     return (
         <>
             <Header />
-            <Search search={search} handleSearchChange={handleSearch} />
-            <section style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
+            <Search search={search} handleSearchChange={handleSearchChange} handleSearch={handleSearch} />
+            <section style={{ display: "flex", flexWrap: "wrap" }}>
                 {filteredMembers.map((member) => (
-                    <Card key={member.id} name={member.name} englishName={member.englishName} github={member.github} />
+                    <Card key={member.id} name={member.name} github={member.github} englishName={member.englishName} />
                 ))}
             </section>
         </>
