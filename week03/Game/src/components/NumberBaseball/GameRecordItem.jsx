7618e171d050c120875b53fbf1d27812b1d756fb
KIM JEONGMIN <wjdals030328@catholic.ac.kr>
2025-05-01 15:36:18 +0900

    style:야구 ui

---
+++ week03/Game/src/components/NumberBaseball/GameRecordItem.jsx
@@ -0,0 +1,20 @@
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    box-sizing: border-box;
+`;
+const Message = styled.p`
+    font-size: ${({ theme }) => theme.fontSizes.lg};
+    color: ${({ theme }) => theme.colors.text};
+`;
+
+export default function GameRecordItem() {
+    return (
+        <Container>
+            <Message>이전 기록</Message>
+        </Container>
+    );
+}
