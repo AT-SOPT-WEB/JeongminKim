d259c4b2264ad2a50c97b2ed7fccf0fef5d8babe
KIM JEONGMIN <wjdals030328@catholic.ac.kr>
2025-05-05 17:42:44 +0900

    chore: 초기 프로젝트 세팅

---
+++ week04/auth/src/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./theme";
+
+// 페이지 컴포넌트
+import Login from "./pages/public/Login";
+import Signup from "./pages/public/SingUp";
+import NotFound from "./pages/NotFound";
+import DashboardLayout from "./pages/protected/DashboardLayout";
+import Mypage from "./pages/protected/Mypage";
+import Users from "./pages/protected/Users";
+
+function App() {
+    return (
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <Routes>
+                    {/*로그인 전*/}
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
+                    {/*로그인 후*/}
+                    <Route path="/" element={<DashboardLayout />}>
+                        <Route path="mypage" element={<Mypage />} />
+                        <Route path="users" element={<Users />} />
+                    </Route>
+                    {/* 404 */}
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </BrowserRouter>
+        </ThemeProvider>
+    );
+}
+
+export default App;
